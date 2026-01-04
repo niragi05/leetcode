50056d6550740e9501bf2ea612e6df61c4ca4810
niragi05 <niragimasalia@gmail.com>
2026-01-03 23:53:19 -0700

    binary search

---
+++ BinarySearch/searchInsert.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            if (nums[mid] > target ):
+                end = mid - 1
+            elif (nums[mid] < target):
+                start = mid + 1
+            else:
+                return mid
+        return start
+        
